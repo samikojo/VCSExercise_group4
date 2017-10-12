4962899842fec40a3d49e5cc8d5975a26927b247
unknown <c6tonsun@KC411B17.tamk.ad>
2017-10-12 10:39:02 +0300

    Added jump.

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,35 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
+		[SerializeField]
+		private float _jumpForce;
+
+		private bool _isOnGround;
+		private Rigidbody _rb;
+
+		void Start(){
+			_rb = gameObject.GetComponent<Rigidbody> ();
+		}
+
 		void Update()
 		{
+			if (Input.GetKeyDown (KeyCode.Space) && _isOnGround) {
+				_rb.AddForce (Vector3.up * _jumpForce);
+				_isOnGround = false;
+			}
 		}
+
+		private void OnCollisionEnter(Collision other){
+			if (other.gameObject.name == "Plane") {
+				_isOnGround = true;
+			}
+		}
+		/*
+		private void OnCollisionStay(Collision other){
+			if (!_isOnGround && other.gameObject.name == "Plane") {
+				_isOnGround = true;
+			}
+		}
+		*/
 	}
 }
