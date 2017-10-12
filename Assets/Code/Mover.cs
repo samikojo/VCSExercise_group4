038632a4673511ccebb24b90084c0118ac58089b
BitesTheDust <2jaybel@gmail.com>
2017-10-12 10:38:43 +0300

    Mover : Position

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,26 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
-		void Update()
+        protected const string horAxis = "Horizontal";
+        protected const string verAxis = "Vertical";
+
+        private Vector3 moveVector;
+
+        void Update()
 		{
+            moveVector = GetMovement();
+
+            transform.Translate(moveVector);
 		}
+
+        private Vector3 GetMovement()
+        {
+            float horizontal = Input.GetAxis(horAxis);
+            float vertical = Input.GetAxis(verAxis);
+
+            Vector3 newVector = new Vector3(horizontal, 0, vertical);
+
+            return newVector;
+        }
 	}
 }
